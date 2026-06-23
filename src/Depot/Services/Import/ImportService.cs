91e93e99f8390f55228e513d82fa06eb9c72beca
Dave Beusing <david.beusing@gmail.com>
2026-06-23 09:24:10 +0200

    Add Excel import infrastructure

---
+++ src/Depot/Services/Import/ImportService.cs
@@ -0,0 +1,21 @@
+// Copyright (c) 2026 David Beusing
+// Licensed under the MIT License.
+
+using Depot.Models;
+
+namespace Depot.Services.Import;
+
+public sealed class ImportService
+{
+	public ImportPreview CreatePreview(
+		string filePath)
+	{
+		throw new NotImplementedException();
+	}
+
+	public ImportResult ExecuteImport(
+		ImportPreview preview)
+	{
+		throw new NotImplementedException();
+	}
+}
