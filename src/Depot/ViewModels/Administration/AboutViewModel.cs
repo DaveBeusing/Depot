4bf9fba0a38358c2a9d6936e3e6780933ddddd30
Dave Beusing <david.beusing@gmail.com>
2026-08-04 14:00:13 +0200

    Bugfix ui : align administration and workflow pages with global design system

--- src/Depot/ViewModels/Administration/AboutViewModel.cs
+++ src/Depot/ViewModels/Administration/AboutViewModel.cs
@@ -1,6 +1,9 @@
 // Copyright (c) 2026 David Beusing
 // Licensed under the MIT License.
 
+using System.Diagnostics;
+
+using Depot.Commands;
 using Depot.Models;
 using Depot.Services;
 
@@ -11,11 +14,28 @@ public sealed class AboutViewModel : BaseViewModel
 	public AboutViewModel(ApplicationInformationService applicationInformationService)
 	{
 		Information = applicationInformationService.GetVersionInfo();
+		OpenRepositoryCommand = new RelayCommand(OpenRepository, CanOpenRepository);
 	}
 
 	public ApplicationVersionInfo Information { get; }
+	public RelayCommand OpenRepositoryCommand { get; }
 
 	public string VersionBadgeText => $"Version {Information.Version}";
 
 	public string DatabaseSchemaDisplay => $"Schema {Information.DatabaseSchemaVersion}";
+
+	private bool CanOpenRepository() => Uri.TryCreate(Information.RepositoryUrl, UriKind.Absolute, out _);
+
+	private void OpenRepository()
+	{
+		if (!Uri.TryCreate(Information.RepositoryUrl, UriKind.Absolute, out var uri)) return;
+		try
+		{
+			Process.Start(new ProcessStartInfo(uri.AbsoluteUri) { UseShellExecute = true });
+		}
+		catch (Exception exception)
+		{
+			FailOperation(exception, "The Depot repository could not be opened");
+		}
+	}
 }
