b004f07d5176402cacecd291631a3e171180ea37
Dave Beusing <david.beusing@gmail.com>
2026-08-17 22:09:28 +0200

    Refactor ui : complete workspace shell polish

--- src/Depot/Controls/WorkspaceTabControl.cs
+++ src/Depot/Controls/WorkspaceTabControl.cs
@@ -13,22 +13,38 @@ namespace Depot.Controls;
 public sealed class WorkspaceTabControl : TabControl
 {
 	public static readonly DependencyProperty ActiveItemProperty = DependencyProperty.Register(
-		nameof(ActiveItem),
-		typeof(ShellNavigationItem),
-		typeof(WorkspaceTabControl),
+		nameof(ActiveItem), typeof(ShellNavigationItem), typeof(WorkspaceTabControl),
 		new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnActiveItemChanged));
 
 	public static readonly RoutedCommand CloseTabCommand = new(nameof(CloseTabCommand), typeof(WorkspaceTabControl));
+	public static readonly RoutedCommand CloseOtherTabsCommand = new(nameof(CloseOtherTabsCommand), typeof(WorkspaceTabControl));
+	public static readonly RoutedCommand CloseTabsToRightCommand = new(nameof(CloseTabsToRightCommand), typeof(WorkspaceTabControl));
 
 	public WorkspaceTabControl()
 	{
 		CommandBindings.Add(new CommandBinding(CloseTabCommand, OnCloseTabExecuted, OnCloseTabCanExecute));
+		CommandBindings.Add(new CommandBinding(CloseOtherTabsCommand, OnCloseOtherTabsExecuted, OnCloseOtherTabsCanExecute));
+		CommandBindings.Add(new CommandBinding(CloseTabsToRightCommand, OnCloseTabsToRightExecuted, OnCloseTabsToRightCanExecute));
 	}
 
 	public ShellNavigationItem? ActiveItem
 	{
 		get => (ShellNavigationItem?)GetValue(ActiveItemProperty);
 		set => SetValue(ActiveItemProperty, value);
+	}
+
+	public void CloseActiveTab()
+	{
+		if (SelectedItem is ShellNavigationItem item) Close(item);
+	}
+
+	public void SelectRelativeTab(int offset)
+	{
+		if (Items.Count < 2) return;
+		var index = SelectedIndex < 0 ? 0 : SelectedIndex;
+		index = (index + offset + Items.Count) % Items.Count;
+		SelectedIndex = index;
+		if (SelectedItem is ShellNavigationItem item) SetCurrentValue(ActiveItemProperty, item);
 	}
 
 	protected override void OnSelectionChanged(SelectionChangedEventArgs e)
@@ -64,6 +80,39 @@ public sealed class WorkspaceTabControl : TabControl
 	private void OnCloseTabExecuted(object sender, ExecutedRoutedEventArgs e)
 	{
 		if (e.Parameter is ShellNavigationItem item) Close(item);
+		e.Handled = true;
+	}
+
+	private void OnCloseOtherTabsCanExecute(object sender, CanExecuteRoutedEventArgs e)
+	{
+		e.CanExecute = e.Parameter is ShellNavigationItem item && Items.Contains(item) && Items.Count > 1;
+		e.Handled = true;
+	}
+
+	private void OnCloseOtherTabsExecuted(object sender, ExecutedRoutedEventArgs e)
+	{
+		if (e.Parameter is not ShellNavigationItem keep) return;
+		foreach (var item in Items.OfType<ShellNavigationItem>().Where(item => !ReferenceEquals(item, keep)).ToArray()) Items.Remove(item);
+		SelectedItem = keep;
+		SetCurrentValue(ActiveItemProperty, keep);
+		e.Handled = true;
+	}
+
+	private void OnCloseTabsToRightCanExecute(object sender, CanExecuteRoutedEventArgs e)
+	{
+		if (e.Parameter is not ShellNavigationItem item) { e.CanExecute = false; return; }
+		var index = Items.IndexOf(item);
+		e.CanExecute = index >= 0 && index < Items.Count - 1;
+		e.Handled = true;
+	}
+
+	private void OnCloseTabsToRightExecuted(object sender, ExecutedRoutedEventArgs e)
+	{
+		if (e.Parameter is not ShellNavigationItem item) return;
+		var index = Items.IndexOf(item);
+		while (Items.Count > index + 1) Items.RemoveAt(Items.Count - 1);
+		SelectedItem = item;
+		SetCurrentValue(ActiveItemProperty, item);
 		e.Handled = true;
 	}
 
--- src/Depot/MainWindow.xaml.cs
+++ src/Depot/MainWindow.xaml.cs
@@ -4,9 +4,7 @@
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
 
-using Depot.Controls;
 using Depot.Services;
 using Depot.Services.Help;
 using Depot.ViewModels;
@@ -15,29 +13,21 @@ namespace Depot;
 
 public partial class MainWindow : Window
 {
-	private const string UserIconData =
-		"M12,12 C15.3,12 18,9.3 18,6 C18,2.7 15.3,0 12,0 C8.7,0 6,2.7 6,6 C6,9.3 8.7,12 12,12 M2,24 C2,18.5 6.5,14 12,14 C17.5,14 22,18.5 22,24";
-	private const string NotificationIconData =
-		"M4,15 L16,15 M6,15 L6,9 C6,5.7 7.8,3 10,3 C12.2,3 14,5.7 14,9 L14,15 M8,18 L12,18";
+	private const string UserIconData = "M12,12 C15.3,12 18,9.3 18,6 C18,2.7 15.3,0 12,0 C8.7,0 6,2.7 6,6 C6,9.3 8.7,12 12,12 M2,24 C2,18.5 6.5,14 12,14 C17.5,14 22,18.5 22,24";
+	private const string NotificationIconData = "M4,15 L16,15 M6,15 L6,9 C6,5.7 7.8,3 10,3 C12.2,3 14,5.7 14,9 L14,15 M8,18 L12,18";
+	private const string HelpIconData = "M10,18 A8,8 0 1 0 10,2 A8,8 0 1 0 10,18 M7.8,7.2 C8,5.8 9,5 10.3,5 C11.8,5 12.8,5.9 12.8,7.2 C12.8,8.2 12.2,8.8 11.2,9.5 C10.4,10.1 10,10.7 10,11.8 M10,14.5 L10,14.6";
 
 	private readonly CurrentUserViewModel _currentUserViewModel;
 	private readonly ShellNavigationItem _currentUserNavigationItem;
 	private ShellNavigationItem? _notificationNavigationItem;
+	private ShellNavigationItem? _helpNavigationItem;
 	private MainViewModel? _observedViewModel;
 
 	public MainWindow(IAuthorizationService authorization)
 	{
-		var user = authorization.CurrentUser
-			?? throw new InvalidOperationException("A signed-in user is required to open the main window.");
-
+		var user = authorization.CurrentUser ?? throw new InvalidOperationException("A signed-in user is required to open the main window.");
 		_currentUserViewModel = new CurrentUserViewModel(user);
-		_currentUserNavigationItem = new ShellNavigationItem(
-			"User",
-			UserIconData,
-			() => _currentUserViewModel,
-			(_, _) => Task.CompletedTask,
-			"getting-started.first-login");
-
+		_currentUserNavigationItem = new ShellNavigationItem("User", UserIconData, () => _currentUserViewModel, (_, _) => Task.CompletedTask, "getting-started.first-login");
 		InitializeComponent();
 		DataContextChanged += OnDataContextChanged;
 		Loaded += OnLoaded;
@@ -52,14 +42,29 @@ public partial class MainWindow : Window
 		DataContextChanged -= OnDataContextChanged;
 		ObserveViewModel(null);
 		_notificationNavigationItem?.Dispose();
+		_helpNavigationItem?.Dispose();
 		_currentUserNavigationItem.Dispose();
 		base.OnClosed(e);
 	}
 
-	private void OnLoaded(object sender, RoutedEventArgs e) => ObserveViewModel(DataContext as MainViewModel);
+	protected override void OnPreviewKeyDown(KeyEventArgs e)
+	{
+		base.OnPreviewKeyDown(e);
+		if ((Keyboard.Modifiers & ModifierKeys.Control) == 0) return;
+		if (e.Key == Key.W)
+		{
+			WorkspaceTabs.CloseActiveTab();
+			e.Handled = true;
+		}
+		else if (e.Key == Key.Tab)
+		{
+			WorkspaceTabs.SelectRelativeTab((Keyboard.Modifiers & ModifierKeys.Shift) != 0 ? -1 : 1);
+			e.Handled = true;
+		}
+	}
 
-	private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e) =>
-		ObserveViewModel(e.NewValue as MainViewModel);
+	private void OnLoaded(object sender, RoutedEventArgs e) => ObserveViewModel(DataContext as MainViewModel);
+	private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e) => ObserveViewModel(e.NewValue as MainViewModel);
 
 	private void ObserveViewModel(MainViewModel? viewModel)
 	{
@@ -72,31 +77,16 @@ public partial class MainWindow : Window
 	private void OnMainViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
 	{
 		if (e.PropertyName != nameof(MainViewModel.CurrentViewModel) || sender is not MainViewModel viewModel) return;
-		if (!ReferenceEquals(viewModel.CurrentViewModel, viewModel.NotificationCenterViewModel)) return;
-
-		_notificationNavigationItem ??= new ShellNavigationItem(
-			"Notifications",
-			NotificationIconData,
-			() => viewModel.NotificationCenterViewModel,
-			(_, _) => Task.CompletedTask,
-			"getting-started.first-login");
-
-		if (FindVisualChild<WorkspaceTabControl>(this) is { } workspaceTabs &&
-			!ReferenceEquals(workspaceTabs.ActiveItem, _notificationNavigationItem))
+		if (ReferenceEquals(viewModel.CurrentViewModel, viewModel.NotificationCenterViewModel))
 		{
-			workspaceTabs.ActiveItem = _notificationNavigationItem;
+			_notificationNavigationItem ??= new ShellNavigationItem("Notifications", NotificationIconData, () => viewModel.NotificationCenterViewModel, (_, _) => Task.CompletedTask, "getting-started.first-login");
+			WorkspaceTabs.ActiveItem = _notificationNavigationItem;
 		}
-	}
-
-	private static T? FindVisualChild<T>(DependencyObject parent) where T : DependencyObject
-	{
-		for (var index = 0; index < VisualTreeHelper.GetChildrenCount(parent); index++)
+		else if (ReferenceEquals(viewModel.CurrentViewModel, viewModel.HelpViewModel))
 		{
-			var child = VisualTreeHelper.GetChild(parent, index);
-			if (child is T match) return match;
-			if (FindVisualChild<T>(child) is { } descendant) return descendant;
+			_helpNavigationItem ??= new ShellNavigationItem("Help", HelpIconData, () => viewModel.HelpViewModel, (_, _) => Task.CompletedTask, HelpService.FallbackTopicId);
+			WorkspaceTabs.ActiveItem = _helpNavigationItem;
 		}
-		return null;
 	}
 
 	private void OnWindowActivated(object? sender, EventArgs e)
@@ -111,8 +101,7 @@ public partial class MainWindow : Window
 
 	private async void OnCurrentUserClick(object sender, RoutedEventArgs e)
 	{
-		if (DataContext is MainViewModel viewModel)
-			await viewModel.NavigateAsync(_currentUserNavigationItem);
+		if (DataContext is MainViewModel viewModel) await viewModel.NavigateAsync(_currentUserNavigationItem);
 	}
 
 	private async void OnOpenHelpExecuted(object sender, ExecutedRoutedEventArgs e)
