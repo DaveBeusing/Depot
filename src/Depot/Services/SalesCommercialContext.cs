e3509b4fbcb23480edbaa73f958c8a2158f2c37c
Dave Beusing <david.beusing@gmail.com>
2026-08-20 18:38:13 +0200

    Refactor sales : complete commercial ui context

--- src/Depot/Services/SalesCommercialContext.cs
+++ src/Depot/Services/SalesCommercialContext.cs
@@ -5,33 +5,40 @@ namespace Depot.Services;
 
 internal static class SalesCommercialContext
 {
-	public static CustomerService Customers { get; private set; } = null!;
 	public static SalesPricingService Pricing { get; private set; } = null!;
 	public static SalesTimelineService Timeline { get; private set; } = null!;
 	public static SalesQuoteService Quotes { get; private set; } = null!;
 	public static ShipmentPackingService Packing { get; private set; } = null!;
+	public static CustomerService Customers { get; private set; } = null!;
 	public static ItemService Items { get; private set; } = null!;
 	public static IFileDialogService FileDialogs { get; private set; } = null!;
-	public static SalesDocumentService Documents { get; } = new();
-	public static SalesDocumentEmailService Email { get; } = new();
-	public static bool DomainConfigured { get; private set; }
-	public static bool UiConfigured { get; private set; }
+	public static SalesDocumentEmailService Email { get; private set; } = new();
+	public static SalesDocumentService Documents { get; private set; } = new();
+	public static bool IsConfigured { get; private set; }
+	public static bool IsUiConfigured { get; private set; }
 
-	public static void Configure(SalesPricingService pricing, SalesTimelineService timeline, SalesQuoteService quotes, ShipmentPackingService packing)
+	public static void Configure(
+		SalesPricingService pricing,
+		SalesTimelineService timeline,
+		SalesQuoteService quotes,
+		ShipmentPackingService packing,
+		SalesDocumentService? documents = null,
+		SalesDocumentEmailService? email = null)
 	{
 		Pricing = pricing;
 		Timeline = timeline;
 		Quotes = quotes;
 		Packing = packing;
-		DomainConfigured = true;
+		Documents = documents ?? Documents;
+		Email = email ?? Email;
+		IsConfigured = true;
 	}
 
-	public static void ConfigureCustomer(CustomerService customers) => Customers = customers;
-
-	public static void ConfigureUi(ItemService items, IFileDialogService fileDialogs)
+	public static void ConfigureUi(CustomerService customers, ItemService items, IFileDialogService fileDialogs)
 	{
+		Customers = customers;
 		Items = items;
 		FileDialogs = fileDialogs;
-		UiConfigured = true;
+		IsUiConfigured = true;
 	}
 }
