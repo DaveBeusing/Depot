ca88bab23d889ac42da7e332f8bf91afce070799
Dave Beusing <david.beusing@gmail.com>
2026-08-24 16:23:57 +0200

    Synchronize sales order activation

--- src/Depot/Views/SalesOrdersView.xaml.cs
+++ src/Depot/Views/SalesOrdersView.xaml.cs
@@ -1,11 +1,30 @@
 // Copyright (c) 2026 David Beusing
 // Licensed under the MIT License.
 
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+using Depot.Models;
+using Depot.ViewModels;
 
 namespace Depot.Views;
 
 public partial class SalesOrdersView : UserControl
 {
-	public SalesOrdersView() => InitializeComponent();
+	public SalesOrdersView()
+	{
+		InitializeComponent();
+		AddHandler(Selector.SelectionChangedEvent, new SelectionChangedEventHandler(OnSelectionChanged));
+	}
+
+	private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+	{
+		if (DataContext is not SalesOrdersViewModel viewModel || e.OriginalSource is not ListBox listBox) return;
+		if (!ReferenceEquals(listBox.ItemsSource, viewModel.Workspace.Orders)) return;
+
+		var selectedOrder = listBox.SelectedItem as SalesOrder;
+		if (!ReferenceEquals(viewModel.Workspace.SelectedOrder, selectedOrder)) viewModel.Workspace.SelectedOrder = selectedOrder;
+		if (!ReferenceEquals(viewModel.SelectedOrder, selectedOrder)) viewModel.SelectedOrder = selectedOrder;
+	}
 }
