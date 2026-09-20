43b7df8ceb1d7d3c5faa4640e4b80bd8e8fe4e92
Dave Beusing <david.beusing@gmail.com>
2026-09-20 11:11:07 +0200

    Reduce unnecessary collection refresh work

--- src/Depot/ViewModels/CollectionSynchronizer.cs
+++ src/Depot/ViewModels/CollectionSynchronizer.cs
@@ -10,7 +10,14 @@ internal static class CollectionSynchronizer
 	public static void Replace<T>(ObservableCollection<T> target, IReadOnlyList<T> values)
 	{
 		var sharedCount = Math.Min(target.Count, values.Count);
-		for (var index = 0; index < sharedCount; index++) target[index] = values[index];
+		var comparer = EqualityComparer<T>.Default;
+		for (var index = 0; index < sharedCount; index++)
+		{
+			var current = target[index];
+			var next = values[index];
+			if (ReferenceEquals(current, next) || comparer.Equals(current, next)) continue;
+			target[index] = next;
+		}
 		while (target.Count > values.Count) target.RemoveAt(target.Count - 1);
 		for (var index = sharedCount; index < values.Count; index++) target.Add(values[index]);
 	}
